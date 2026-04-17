ee0899c137ddd8ee882f463b1596c70fdae7ca1b
GynGo-AST <augusto.schaumburg@gmail.com>
2026-04-17 16:08:52 -0300

    Aplicacion Saludo REGISTRADA

--- miproyecto/miproyecto/settings.py
+++ miproyecto/miproyecto/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-#    'saludo', # la app saludo debe registrarse.
+    'saludo', # la app saludo debe registrarse.
 ]
 
 MIDDLEWARE = [
