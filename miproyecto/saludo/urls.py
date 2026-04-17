f93f0efe21c52cd3e803922ab7171e58ddc1061e
GynGo-AST <augusto.schaumburg@gmail.com>
2026-04-17 16:07:53 -0300

    Aplicacion saludo, anda pero no es correcto

---
+++ miproyecto/saludo/urls.py
@@ -0,0 +1,9 @@
+form django.urls import path
+
+# importación de las vistas
+form .views import hola_mundo
+
+# configuración de las rutas del módulo
+urlpatterns = [
+    path('', hola_mundo),
+]
--- miproyecto/saludo/views.py
+++ miproyecto/saludo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+# importación necesaria
+from django.http import HttpResponse
 
 # Create your views here.
+
+# primer método de la vista
+def hola_mundo(request):
+    return HttpResponse("Hola Mundo!!!")
+
