4bc7db02a3ba7de8633a59df43f46984a47b030f
GynGo-AST <augusto.schaumburg@gmail.com>
2026-04-17 17:49:55 -0300

    app/view.py

--- miproyecto/app/views.py
+++ miproyecto/app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from app.models import Users, Role
+from app.serializers import UserSerializer, RoleSerializer
+
 
 # Create your views here.
+class UserView(APIView):
+
+    def get(self, request):
+        users = Users.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+      
