8c03e5ac3bf1822d8b50d695d91847adbb39f0c5
GynGo-AST <augusto.schaumburg@gmail.com>
2026-04-20 11:43:51 -0300

    app/views.py creando vistas

--- miproyecto/app/views.py
+++ miproyecto/app/views.py
@@ -12,5 +12,24 @@ class UserView(APIView):
         users = Users.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        seializer = UserSerializer(data=request.data)
+        if seializer.is_valid():
+            seializer.save()
+            return Response(seializer.data, status=201)
+        return Response(seializer.errors, status=400)
+
+class RoleView(APIView):
+    def get(self, request):
+        roles = Role.objects.all()
+        serializer = RoleSerializer(roles, many=True)
+        return Response(serializer.data)
     
-      
+    def post(self, request):
+        serializer = RoleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
