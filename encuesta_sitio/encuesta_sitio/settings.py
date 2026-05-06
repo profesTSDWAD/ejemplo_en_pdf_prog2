ebda170689a145be3524248ee62a96943eecef4e
GynGo-AST <augusto.schaumburg@gmail.com>
2026-05-06 16:59:25 -0300

    ejemplo encuesta de Django

--- encuesta_sitio/encuesta_sitio/settings.py
+++ encuesta_sitio/encuesta_sitio/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
+    'polls.apps.PollsConfig',
+    # 'polls',
 ]
 
 MIDDLEWARE = [
